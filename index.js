bb077045e0c5892dad4f89e710ba76e4209c919f
shaheer123 <104161622+DANGEOUR@users.noreply.github.com>
2024-03-22 12:04:19 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+setTimeout(function () {
+    $('.loader_bg').fadeToggle();
+}, 1500);
+
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/65807f2270c9f2407f8124ff/1hhuv18gd';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
